8b4df8026d0e80049d46c7964a075d4c67aa20f9
Sifana p v <sifanapv2001@gmail.com>
2026-09-25 17:15:18 +0530

    Add NASA exoplanet detection project

---
+++ src/config.py
@@ -0,0 +1,21 @@
+# ==========================================
+# NASA EXOPLANET DEPLOYMENT CONFIGURATION
+# ==========================================
+
+FEATURES = [
+    "ra",
+    "dec",
+    "st_pmra",
+    "st_pmdec",
+    "pl_tranmid",
+    "pl_orbper",
+    "pl_trandep",
+    "pl_rade",
+    "pl_insol",
+    "pl_eqt",
+    "st_tmag",
+    "st_dist",
+    "st_teff",
+    "st_logg",
+    "st_rad"
+]
